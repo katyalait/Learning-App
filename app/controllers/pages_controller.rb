432bcbd84e0c677748538bf8d6011ab76daf149b
edvinast <teiserse@tcd.ie>
2018-03-31 12:44:23 +0100

    Added basic queries to display on myLocations menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,9 +27,12 @@ class PagesController < ApplicationController
   end
 
   def myLocationsTwo
+    @dats = params
   end 
 
   def myLocations
+    current_user
+    @userdevice = Device.find_by(_id: @current_user.aware_device_id).device_id
   end
 
   def top_nav
